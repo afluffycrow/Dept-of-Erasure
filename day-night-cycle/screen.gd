35a456bd8e093126b2f50169fa69bd61120a71b6
afluffycrow <102310615+afluffycrow@users.noreply.github.com>
2025-02-15 22:13:26 +0000

    Add files via upload

--- day-night-cycle/screen.gd
+++ day-night-cycle/screen.gd
@@ -2,9 +2,9 @@ extends Node2D
 
 @export var ui = Control
 @export var canvas_modulate = CanvasModulate
-@export var canvas_layer = CanvasLayer
+
 
 func _ready() -> void:
-	canvas_layer.visible = true
+	
 	canvas_modulate.time_out.connect(ui.set_daytime)
 	
